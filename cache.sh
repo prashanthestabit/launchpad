6daa0d29920c205fb2b8f5c095c880b37230b100
prashanthestabit <prashantpandey@hestabit.com>
2023-01-23 15:32:35 +0530

    create cache sh file for cache clear

---
+++ cache.sh
@@ -0,0 +1,10 @@
+php artisan optimize;
+php artisan config:clear;
+php artisan route:clear;
+php artisan view:clear;
+php artisan event:clear;
+php artisan route:cache;
+php artisan config:cache
+php artisan cache:clear;
+php artisan optimize:clear;
+php artisan schedule:clear-cache;
